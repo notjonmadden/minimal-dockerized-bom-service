4ea23fabc1cc19f3da7c2eb31850722901f919aa
Jon Madden <notjonmadden@gmail.com>
2018-01-29 19:12:53 -0500

    initial commit

---
+++ Controllers/AssembliesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalDockerizedBomService.Controllers
+{
+    [Route("[controller]")]
+    public class AssembliesController : Controller
+    {
+        [HttpGet("{partNumber}")]
+        public ActionResult Get(string partNumber)
+        {
+            if ("E11401906".Equals(partNumber))
+            {
+                return Ok(new { Description = "VALVE" });
+            }
+
+            return NotFound();
+        }
+    }
+}
